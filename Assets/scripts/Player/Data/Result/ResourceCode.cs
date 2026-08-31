43d9e90f2aaf481ed67546de83fa953a8eba99a7
Atwelve <ppwqhu@gmail.com>
2026-08-31 19:24:08 +0800

    修改不需要的回退

---
+++ Assets/scripts/Player/Data/Result/ResourceCode.cs
@@ -0,0 +1,7 @@
+﻿public class ResourceCode
+{
+    //成功
+    public const int SUCCESS = 200;
+    //背包满
+    public const int FULL = 1;
+}
---
+++ Assets/scripts/Player/Resource/ResourcePickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class ResourcePickup : MonoBehaviour
+{
+    //拾取物对应的资源ID，Inspector面板配置，默认1001
+    public int resourceID = 1001;
+    //拾取后获得的资源数量
+    public int amount = 1;
+    //背包系统
+    public ResourceSystemHost resourceSystem;
+    //资源管理器
+    private ResourceManager manager;
+    //2D触发器进入事件
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Player"))
+        {  return; }
+        manager = resourceSystem.Manager;
+        Debug.Log("拾取资源ID：" + resourceID +"数量："+amount);
+        manager.RequestAddResource(resourceID,amount);
+        Destroy(gameObject);
+    }
+}
