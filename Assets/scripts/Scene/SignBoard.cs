f0a72e6833e23cb2ad979ca13721d7f375f92a1e
sanxiaojijiaben <sanxiaojijiaben@gmail.com>
2026-09-01 23:38:58 +0800

    feat(GameScene): 血条动态变化、防掉落碰撞体、木板提示预制体
    - 实现血条跟随角色血量实时变化
    - 添加碰撞体防止角色从边缘掉落
    - 新增木板触发提示文字预制体

---
+++ Assets/scripts/Scene/SignBoard.cs
@@ -0,0 +1,44 @@
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("提示文字")]
+    public string tipText;
+
+    [Header("UI提示文本组件")]
+    public TMP_Text uiTipText;
+
+    private bool _isShow = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag ("Player")&& !_isShow)
+        {
+            ShowTip();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other .CompareTag ("Player"))
+        {
+            HideTip();
+        }
+    }
+
+    void ShowTip()
+    {
+        _isShow = true;
+        uiTipText.text = tipText;
+        uiTipText.gameObject.SetActive(true);
+    }
+    void HideTip()
+    {
+        _isShow = false;
+        uiTipText.gameObject.SetActive(false);
+    }
+}
